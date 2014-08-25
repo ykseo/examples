3aea9bb62d70bf7eb4c3eb8f05699ab6ea3a0685
YK <yk@p3227-ipbf5104marunouchi.tokyo.ocn.ne.jp>
2014-08-26 00:06:01 +0900

    ()랑 “” 해결

--- helloWorld/ViewController.m
+++ helloWorld/ViewController.m
@@ -29,7 +29,7 @@
 - (IBAction)uiClickAction:(UIButton *)sender {
     _uiTitle.text = [NSString stringWithFormat:@"%d단 구구단은 이렇게",[_inputBox.text intValue]];
     int input = [_inputBox.text intValue];
-    
+    NSString *outItem = [NSString stringWithFormat:@""];
     int gugu = 0;
     //배열을 선언 9행짜리
     NSMutableArray *text = [NSMutableArray arrayWithCapacity:9];
@@ -37,7 +37,13 @@
     for(int j=1; j<=9; j++){
         gugu = input*j;
         //배열에 넣어
-        [text addObject:[NSString stringWithFormat:@"%d * %d = %d",input, j, gugu]];
+        //[text addObject:[NSString stringWithFormat:@"%d * %d = %d",input, j, gugu]];
+        outItem = [outItem stringByAppendingFormat:@"%d * %d = %d",input, j, gugu];
+        outItem = [outItem stringByAppendingFormat:@"\n"];
+        //NSLog(@"%@",outItem);
+      
+        
+
     }
     
     /*배열이 잘들어갔는지 로그에 함 찍어보세
@@ -48,7 +54,8 @@
     //[goOutputGugu input];
     
     //배열을 테스트 뷰에 뿌려라!
-    _outBox.text=text.description;
+    _outBox.text = outItem;
+    //_outBox.text=text.description;
     //_outBox.text = text.debugDescription;
     //_outTableView.t = text.description;
     //텍스트 뷰의 길이가 짧을수있으니깐 스크롤을 만들어주자
